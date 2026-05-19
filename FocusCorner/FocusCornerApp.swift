09dd97d05061cdd888bb748446347d07aeca6fbc
Meryem Demir <meryemdmirr0@gmail.com>
2026-05-19 18:02:16 +0300

    Initial Commit

---
+++ FocusCorner/FocusCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusCornerApp.swift
+//  FocusCorner
+//
+//  Created by Meryem Demir on 19.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct FocusCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
